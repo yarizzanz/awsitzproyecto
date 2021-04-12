c2c9ac8755ce3cc35effe319b91d1d956177e492
yarizzanz <65185570+yarizzanz@users.noreply.github.com>
2021-04-12 18:28:55 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,10 @@
-
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_R0CNpRU8H', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7h0itj2sl7an99ibg162ibt9tl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
